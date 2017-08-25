ead6cb4b515632e0d064e8c7202a3fc03a164fef
preston <broberts9@gmail.com>
2017-08-24 23:33:49 -0700

    init push

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
+console.log("your app is listening on 8080...");
