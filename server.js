b07378cc3ca5d5b2acca284b0e9f23139fbfba64
preston <broberts9@gmail.com>
2017-08-27 23:38:06 -0700

    created endpoints for job search and job post; created tests for new endpoints

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
 
+// express static middleware
 app.use(express.static('public'));
 
+// get root
 app.get('/', (request, response) => {
     response.sendFile('/index.html');
 });
 
+// get job search page
+app.get('/find', (req, res) => {
+    res.sendFile(__dirname + '/public/find.html');
+});
+
+// get job submit page
+app.get('/submit', (req, res) => {
+    res.sendFile(__dirname + '/public/submit.html');
+});
 
 
-
-
-
-
-
-app.listen(process.env.PORT || 8080);
-
-console.log("your app is listening on 8080...");
+app.listen(process.env.PORT || 8080, () => {
+    let dateTime = new Date();
+    let hourMinute = dateTime.getHours() + ":" + dateTime.getMinutes();
+    
+    console.log(hourMinute + " I'm listening on 8080...");
+});
 
 module.exports = { app };
--- test/test-server.js
+++ test/test-server.js
@@ -8,9 +8,27 @@ const { app } = require('../server')
 chai.use(chaiHttp);
 
 describe('server', function () {
-    it('should test for 200s and html', function () {
+    it('should test for 200s and html when getting root', function () {
         return chai.request(app)
             .get('/')
+            .then(function (response) {
+                response.should.have.status(200);
+                response.should.be.html;
+            });
+    });
+
+    it('should test for 200s and html when getting job search page', function () {
+        return chai.request(app)
+            .get('/find')
+            .then(function (response) {
+                response.should.have.status(200);
+                response.should.be.html;
+            });
+    });
+
+    it('should test for 200s and html when getting job submit page', function () {
+        return chai.request(app)
+            .get('/submit')
             .then(function (response) {
                 response.should.have.status(200);
                 response.should.be.html;
