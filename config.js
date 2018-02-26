bc18952adbde0509d79aedf90edf0cf2503bf6e8
preston <broberts9@gmail.com>
2018-02-26 14:32:18 -0800

    configuring dotenv file

--- config.js
+++ config.js
@@ -1,12 +1,8 @@
 exports.DATABASE_URL =
-    process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
-    'mongodb://localhost/gigFinder';
-// 'mongodb://preston:THINKful999@ds161194.mlab.com:61194/gigfinder';
+    process.env.DATABASE_URL || 'mongodb://localhost/gigFinder';
 
 exports.TEST_DATABASE_URL =
-    process.env.TEST_DATABASE_URL ||
-    'mongodb://localhost/gigFinderTestDB';
+    process.env.TEST_DATABASE_URL || 'mongodb://localhost/gigFinderTestDB';
 
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'idontknow';
